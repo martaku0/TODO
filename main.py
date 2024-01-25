f0294cad4a0ee643ff7336f592c80900cf9c774f
martaku0 <74990353+martaku0@users.noreply.github.com>
2024-01-25 11:35:05 +0100

    almost done

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import sys
+import sqlite3
 import datetime
 from functools import partial
 
@@ -7,7 +8,7 @@ from PySide6.QtWidgets import (QApplication, QMainWindow, QLabel,
                                QScrollArea, QDialog, QDialogButtonBox, QLineEdit,
                                QMessageBox, QDateTimeEdit)
 from PySide6.QtCore import QSize, Qt, QTimer
-from PySide6.QtGui import QFont
+from PySide6.QtGui import QFont, QPalette
 
 class AddNewTaskDialog(QDialog):
     def __init__(self, tNow):
@@ -89,6 +90,7 @@ class MainWindow(QMainWindow):
         sidebar = QVBoxLayout()
         addTaskButton = QPushButton("Add new task")
         addTaskButton.clicked.connect(partial(self.addNewTask, tasks))
+        addTaskButton.setStyleSheet("padding: 10px;")
         sidebar.addWidget(addTaskButton, alignment=Qt.AlignCenter)
 
         clock = QLabel("")
@@ -113,6 +115,11 @@ class MainWindow(QMainWindow):
 
         self.setCentralWidget(central_widget)
 
+        timertask = QTimer(self)
+        timertask.timeout.connect(partial(self.loadTasks, tasks))
+        timertask.start(1000)
+        self.loadTasks(tasks)
+
     def timeNow(self):
         now = datetime.datetime.now()
         time = now.strftime("%Y-%m-%d %H:%M:%S")
@@ -124,38 +131,158 @@ class MainWindow(QMainWindow):
     def addNewTask(self, tasks):
         dialog = AddNewTaskDialog(self.timeNow())
         if dialog.exec():
-            task = Task(dialog.getData()["title"], dialog.getData()["description"], self.timeNow(), dialog.getData()['endTime'])
-            tasks.addWidget(task, alignment=Qt.AlignTop)
+            t = dialog.getData()["title"]
+            d = dialog.getData()["description"]
+            end = dialog.getData()['endTime']
+            error = QMessageBox()
+            error.setWindowTitle("Error")
+
+            if(t == ""):
+                error.setText("Title cannot be empty!")
+                error.exec()
+            elif(end < datetime.datetime.now()):
+                error.setText("End time must be in the future!")
+                error.exec()
+            else:
+                start = datetime.datetime.strptime(self.timeNow(), "%Y-%m-%d %H:%M:%S")
+                task = Task(t,d, start, end, 0, 1)
+                task.addToDb()
+                # tasks.addWidget(task, alignment=Qt.AlignTop)
+                self.loadTasks(tasks)
         else:
             print('adding new task canceled')
 
+    def loadTasks(self, tasks):
+        conn = sqlite3.connect('data')
+        cursor = conn.cursor()
 
+        cursor.execute("SELECT * FROM tasks ORDER BY end_time ASC")
+        tasksList = cursor.fetchall()
 
+        while tasks.count():
+            item = tasks.takeAt(0)
+            widget = item.widget()
+            if widget is not None:
+                widget.deleteLater()
+
+        for t in tasksList:
+            start = datetime.datetime.strptime(t[3], "%Y-%m-%d %H:%M:%S")
+            end = datetime.datetime.strptime(t[4], "%Y-%m-%d %H:%M:%S")
+            task = Task(t[1], t[2], start, end, t[0], t[5])
+            tasks.addWidget(task, alignment=Qt.AlignTop)
+
+        cursor.close()
+        conn.close()
 
 class Task(QWidget):
-    def __init__(self, t, d, startT, endT):
+    def __init__(self, t, d, startT, endT, id_, act):
         super().__init__()
+
+        self.id = id_
 
         layout = QVBoxLayout()
 
-        title = QLabel(str(t))
+        now = datetime.datetime.now()
+
+        self.active = act
+
+        titleLbl = QLabel(str(t))
         titleFont = QFont()
         titleFont.setBold(True)
-        title.setFont(titleFont)
-        description = QLabel(str(d))
-        time = QLabel("Start time: " + str(startT) + " | End time: " + endT.strftime("%d-%m-%Y %H:%M:%S"))
+        titleLbl.setFont(titleFont)
+        if(endT >= now and act == 1):
+            titleLbl.setStyleSheet("color: red;")
+        elif(endT < now and act == 1):
+            titleLbl.setStyleSheet("color: green;")
+        descriptioLbl = QLabel(str(d))
+        self.startTime = startT
+        self.endTime = endT
+        self.title = str(t)
+        self.description = str(d)
+
+        time = QLabel("Start time: " + startT.strftime("%Y-%m-%d %H:%M:%S") + " | End time: " + endT.strftime("%Y-%m-%d %H:%M:%S"))
         timeFont = QFont()
         timeFont.setItalic(True)
         time.setFont(timeFont)
 
-        layout.addWidget(title)
-        layout.addWidget(description)
+        layout.addWidget(titleLbl)
+        layout.addWidget(descriptioLbl)
         layout.addWidget(time)
 
-        self.setMaximumHeight(100)
-        self.setMinimumHeight(100)
+        delBtn = QPushButton("Delete")
+        delBtn.clicked.connect(self.delFromDb)
+        delBtn.setFixedSize(100, 25)
+        layout.addWidget(delBtn)
+
+        actBtn = QPushButton("End task")
+        actBtn.clicked.connect(self.endTask)
+        actBtn.setFixedSize(100, 25)
+        layout.addWidget(actBtn)
+
+        bottom = QLabel("")
+        lineFont = QFont()
+        lineFont.setPointSize(1)
+        bottom.setFont(lineFont)
+        bottom.setStyleSheet("background-color: black;")
+        layout.addWidget(bottom)
+
+        self.setMaximumHeight(150)
+        self.setMinimumHeight(150)
 
         self.setLayout(layout)
+
+    def addToDb(self):
+        conn = sqlite3.connect('data')
+        cursor = conn.cursor()
+
+        cursor.execute('''INSERT INTO tasks (title, description, start_time, end_time) values (?, ?, ?, ?)
+        ''', (self.title, self.description, self.startTime, self.endTime))
+
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def delFromDb(self):
+        confirm = QMessageBox()
+        confirm.setWindowTitle("Delete")
+        confirm.setText("Are you sure?")
+        confirm.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+        confirm.setDefaultButton(QMessageBox.No)
+        result = confirm.exec()
+
+        if (result == QMessageBox.Yes):
+            conn = sqlite3.connect('data')
+            cursor = conn.cursor()
+
+            cursor.execute('DELETE FROM tasks WHERE id = ?', (self.id,))
+
+            conn.commit()
+
+            cursor.close()
+            conn.close()
+
+    def endTask(self):
+        if(self.active == 1):
+            confirm = QMessageBox()
+            confirm.setWindowTitle("End")
+            confirm.setText("Are you sure?")
+            confirm.setStandardButtons(QMessageBox.Yes | QMessageBox.No)
+            confirm.setDefaultButton(QMessageBox.No)
+            result = confirm.exec()
+
+            if(result == QMessageBox.Yes):
+                conn = sqlite3.connect('data')
+                cursor = conn.cursor()
+
+                now = datetime.datetime.now()
+                time = now.strftime("%Y-%m-%d %H:%M:%S")
+                cursor.execute('UPDATE tasks SET end_time = ? WHERE id = ?', (time,self.id))
+
+                conn.commit()
+
+                cursor.close()
+                conn.close()
 
 
 app = QApplication()
